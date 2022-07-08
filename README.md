e1b0048c0c7d87dd9df7e543912625a8ac73e596
Seth Kinast <seth@sethkinast.com>
2022-07-07 23:44:14 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Mitsubishi Heat Pump + MQTT Hubitat Driver
+
+This driver is designed to work with [mitsubishi_heatpump_mqtt_esp8266_esp32](https://github.com/SwiCago/HeatPump/tree/master/examples/mitsubishi_heatpump_mqtt_esp8266_esp32) from [SwiCago's HeatPump.cpp library](https://github.com/SwiCago/HeatPump). The library runs on an ESP8266 or ESP32 microcontroller connected to the heatpump via connector CN105 (just like the official Kumo Cloud device).
+
+## Features
+
+- Supports both authenticated and anonymous MQTT connections
+- Supports the Hubitat / Google Home integration so you can view and control the heatpumps with Home / Nest devices
+- Can detect when the heatpumps are actively heating/cooling and update Hubitat Dashboard
+- Reports room temperature back from the heatpump for use in automations
+- Supports all heatpump modes and fan speeds
+
+## Prerequisites
+
+1. Before flashing with the mitsubishi_heatpump_mqtt_esp8266_esp32 sketch, edit the header file and configure `heatpump_topic`, `heatpump_set_topic`, and `heatpump_status_topic`. These topics are used to communicate between the unit and Hubitat.
+2. Flash your ESP8266 / ESP32 device with the sketch and connect to the heatpump.
+3. You need an MQTT broker running on your network. I use `mosquitto` on a Raspberry Pi.
+
+## Installation
+
+1. Add this device driver to Hubitat by going to **Drivers Code -> New Driver -> Import** and pasting [this importURL](https://raw.githubusercontent.com/sethkinast/hubitat-mitsubishi-mqtt/master/hubitat-mitsubishi-mqtt.groovy).
+2. Create a new Virtual Device by going to **Devices -> Add Device -> Virtual** and set the driver to `Mitsubishi Heat Pump + MQTT`.
+3. Edit the device and add the IP address of your MQTT broker and the heatpump topic you configured in the Arduino sketch.
+4. Check the device logs and you should see temperature reports being received and status updates flowing.
+5. (Optional) Expose the heatpump to the Google Home or Hubitat Dashboard apps by opening the apps and adding the heatpump.
+
+## Auto mode
+
+Hubitat thermostats understand the concept of auto mode, but use two separate setpoints for heating and cooling, which isn't how these heat pumps handle auto. I recommend using the standalone heat and cool modes when possible.
+
+## Screenshots
+
+|         Google Home         |         Hubitat Dashboard         |
+| :-------------------------: | :-------------------------------: |
+| ![](images/google_home.png) | ![](images/hubitat_dashboard.png) |
+
+## Troubleshooting
+
+Make sure Debug Logging is on for the device, then check its logs.
+
+- MQTT connection errors mean that the hub can't connect to MQTT. You might need to allow traffic to pass on the MQTT port (1883 by default)
+- If MQTT connects but you get no events from the heatpump, make sure your topic is set to the same topic you used in `mitsubishi_heatpump_mqtt_esp8266_esp32.h`. The settings and status topics are extrapolated from this topic, so if your topic is `climate/office`, you should have set `climate/office`, `climate/office/status`, and `climate/office/set` in the header file.
+- If you're sure that the ESP is on the network, you can turn on debug mode to have it dump every packet it gets so you can verify its behavior. Send a message to the `debug_set_topic` you set in your sketch header:
+
+```bash
+$ mosquitto_pub -t climate/office/debug/set -m on
+```
---
+++ hubitat-mitsubishi-mqtt.groovy
@@ -0,0 +1,422 @@
+/**
+ * Hubitat Device Driver
+ * Mitsubishi Heat Pump + MQTT
+ * v1.0
+ * https://github.com/sethkinast/hubitat-mitsubishi-mqtt/
+ *
+ * Control Mitsubishi heat pumps using HeatPump.cpp via MQTT
+ * More info: https://github.com/SwiCago/HeatPump
+ *
+ * MIT License
+ *
+ * Copyright (c) 2022 Seth Kinast
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+import groovy.transform.Field
+import groovy.json.JsonSlurper
+import groovy.json.JsonOutput
+
+@Field static List<String> supportedThermostatFanModes = ['1', '2', '3', '4', 'auto', 'quiet']
+@Field static List<String> supportedThermostatModes = ['heat', 'cool', 'fan', 'dry', 'auto', 'off']
+
+@Field static Map thermostatModeMapping = [
+    'HEAT': 'heat',
+    'COOL': 'cool',
+    'FAN': 'fan',
+    'DRY': 'dry',
+    'AUTO': 'auto'
+]
+@Field static Map thermostatOperatingStateMapping = [
+    'heat': 'heating',
+    'cool': 'cooling',
+    'dry': 'cooling',
+    'fan': 'fan only',
+    'off': 'idle'
+]
+@Field static Map thermostatFanModeMapping = [
+    'QUIET': 'circulate',
+    '1': 'on',
+    '2': 'on',
+    '3': 'on',
+    '4': 'on',
+    'AUTO': 'auto'
+]
+@Field static Map fanControlSpeedMapping = [
+    'QUIET': 'low',
+    '1': 'medium-low',
+    '2': 'medium',
+    '3': 'medium-high',
+    '4': 'high',
+    'AUTO': 'auto'
+]
+
+metadata {
+    definition(
+        name: 'Mitsubishi Heat Pump + MQTT',
+        namespace: 'cogdev',
+        author: 'Seth Kinast <seth@cogdev.net>',
+        importUrl:
+            'https://raw.githubusercontent.com/sethkinast/hubitat-mitsubishi-mqtt/master/hubitat-mitsubishi-mqtt.groovy'
+    ) {
+        capability 'Configuration'
+        capability 'Initialize'
+        capability 'Thermostat'
+
+        command 'dry'
+    }
+}
+
+preferences {
+    section('MQTT') {
+        input name: 'brokerIP', type: 'string', title: 'MQTT Broker IP Address', required: true
+        input name: 'brokerPort', type: 'string', title: 'MQTT Broker Port', required: true, defaultValue: 1883
+        input name: 'topicRoot', type: 'string', title: 'MQTT Topic Root',
+            description: '(as defined in your header file, e.g. climate/office)', required: true
+        input name: 'brokerUsername', type: 'string', title: 'MQTT User'
+        input name: 'brokerPassword', type: 'password', title: 'MQTT Password'
+    }
+    section('Advanced') {
+        input name: 'debugLoggingEnabled', type: 'bool', title: 'Enable debug logging', defaultValue: true
+    }
+}
+
+void initialize() {
+    if (!getRunningMode()) {
+        configure()
+    }
+
+    connect()
+    if (debugLoggingEnabled) {
+        runIn(3600, disableDebugLogging)
+    }
+}
+
+void configure() {
+    updateRunningMode('cool')
+    sendEvent(name: 'temperature', value: convertTemperatureIfNeeded(68.0, 'F', 1), unit: getTemperatureUnit())
+    sendEvent(name: 'thermostatSetpoint', value: convertTemperatureIfNeeded(70.0, 'F', 1), unit: getTemperatureUnit())
+    sendEvent(name: 'heatingSetpoint', value: convertTemperatureIfNeeded(70.0, 'F', 1), unit: getTemperatureUnit())
+    sendEvent(name: 'coolingSetpoint', value: convertTemperatureIfNeeded(70.0, 'F', 1), unit: getTemperatureUnit())
+    sendEvent(name: 'supportedThermostatFanModes', value: supportedThermostatFanModes)
+    sendEvent(name: 'supportedThermostatModes', value: supportedThermostatModes)
+    sendEvent(name: 'thermostatOperatingState', value: 'idle')
+}
+
+void updated() {
+    initialize()
+}
+
+void parse(String message) {
+    Map parsedMessage = interfaces.mqtt.parseMessage(message)
+    logDebug parsedMessage.toString()
+    String topic = parsedMessage.topic
+    Map payload = new JsonSlurper().parseText(parsedMessage.payload)
+
+    List events
+
+    switch (topic) {
+        case getStatusTopic():
+            events = processTemperatureUpdate(payload)
+            break
+        case topicRoot:
+            events = processOperatingUpdate(payload)
+            break
+    }
+
+    logDebug events.toString()
+    events.each { event ->
+        sendEvent(event)
+    }
+}
+
+List processTemperatureUpdate(Map payload) {
+    BigDecimal temperature = Math.round(
+        new BigDecimal(convertTemperatureIfNeeded(payload.roomTemperature, 'C', 1))
+    )
+    String currentMode = device.currentValue('thermostatMode')
+    return [
+        [
+            name: 'temperature',
+            value: temperature,
+            unit: getTemperatureUnit(),
+            descriptionText: "Temperature is ${temperature}${getTemperatureUnit()}"
+        ],
+        [
+            name: 'thermostatOperatingState',
+            value: currentMode == 'fan' || payload.operating ?
+                    thermostatOperatingStateMapping[currentMode] ?:
+                    thermostatOperatingStateMapping[getRunningMode()] :
+                'idle',
+        ]
+    ]
+}
+
+List processOperatingUpdate(Map payload) {
+    BigDecimal temperature = Math.round(
+        new BigDecimal(convertTemperatureIfNeeded(payload.temperature, 'C', 1))
+    )
+    String mode = getThermostatMode(payload.power, payload.mode)
+    String fanMode = getThermostatFanMode(payload.power, payload.fan)
+
+    List events = [
+        [name: 'thermostatMode', value: mode],
+        [name: 'thermostatFanMode', value: fanMode],
+    ]
+
+    // in FAN/DRY modes the temperature is meaningless
+    switch (mode) {
+        case 'off':
+        case 'fan':
+        case 'dry':
+            // `operating` is always false in these modes
+            events << [name: 'thermostatOperatingState', value: thermostatOperatingStateMapping[mode]]
+            break
+        case 'cool':
+            events << [name: 'coolingSetpoint', value: temperature, unit: getTemperatureUnit()]
+            events << [name: 'thermostatSetpoint', value: temperature, unit: getTemperatureUnit()]
+            updateRunningMode('cool')
+            break
+        case 'heat':
+            events << [name: 'heatingSetpoint', value: temperature, unit: getTemperatureUnit()]
+            events << [name: 'thermostatSetpoint', value: temperature, unit: getTemperatureUnit()]
+            updateRunningMode('heat')
+            break
+        default:
+            events << [name: 'thermostatSetpoint', value: temperature, unit: getTemperatureUnit()]
+            // TODO: read auto mode out of payload and set lastRunningMode
+            break
+    }
+
+    return events
+}
+
+String getThermostatMode(String power, String mode) {
+    return power == 'OFF' ? 'off' : thermostatModeMapping[mode]
+}
+
+String getThermostatFanMode(String power, String mode) {
+    return power == 'OFF' ? 'auto' : thermostatFanModeMapping[mode]
+}
+
+void updateRunningMode(String lastRunningMode) {
+    updateDataValue('lastRunningMode', lastRunningMode)
+}
+
+String getRunningMode() {
+    getDataValue('lastRunningMode')
+}
+
+BigDecimal getSetpointForMode(String mode) {
+    switch (mode) {
+        case 'heat':
+            return device.currentValue('heatingSetpoint')
+        case 'cool':
+            return device.currentValue('coolingSetpoint')
+        case 'auto':
+            return device.currentValue('thermostatSetpoint')
+    }
+}
+
+BigDecimal convertInputToCelsius(BigDecimal inputTemperature) {
+    return getTemperatureScale() == 'C' ?
+        inputTemperature :
+        fahrenheitToCelsius(inputTemperature)
+}
+
+/* Commands */
+void auto() { setThermostatMode('auto') }
+void cool() {
+    sendEvent([name: 'thermostatOperatingState', value: 'pending cool'])
+    setThermostatMode('cool')
+}
+void emergencyHeat() {
+    heat()
+    logDebug 'emergency heat not supported; falling back to heat'
+}
+void heat() {
+    sendEvent([name: 'thermostatOperatingState', value: 'pending heat'])
+    setThermostatMode('heat')
+}
+void dry() { setThermostatMode('dry') }
+void off() { setThermostatMode('off') }
+
+void fanAuto() { setThermostatFanMode('auto') }
+void fanCirculate() { setThermostatFanMode('circulate') }
+void fanOn() { setThermostatFanMode('on') }
+
+void setCoolingSetpoint(BigDecimal setpoint) {
+    sendEvent([name: 'coolingSetpoint', value: setpoint, unit: getTemperatureUnit()])
+    String currentMode = device.currentValue('thermostatMode', true)
+    if (
+        ['pending cool', 'cool'].contains(currentMode) ||
+        currentMode == 'auto' && getRunningMode() == 'cool' // won't work until HeatPump sends x09 payload
+    ) {
+        publish(['temperature': convertInputToCelsius(setpoint)])
+    } else {
+        logDebug "Current mode is ${currentMode} so not publishing coolingSetpoint change to ${setpoint}"
+    }
+}
+
+void setHeatingSetpoint(BigDecimal setpoint) {
+    sendEvent([name: 'heatingSetpoint', value: setpoint, unit: getTemperatureUnit()])
+    String currentMode = device.currentValue('thermostatMode', true)
+    if (
+        ['pending heat', 'heat'].contains(currentMode) ||
+        currentMode == 'auto' && getRunningMode() == 'heat' // won't work until HeatPump sends x09 payload
+    ) {
+        publish(['temperature': convertInputToCelsius(setpoint)])
+    } else {
+        logDebug "Current mode is ${currentMode} so not publishing heatingSetpoint to ${setpoint}"
+    }
+}
+
+void setThermostatMode(String mode) {
+    String mappedMode = thermostatModeMapping.find { m -> m.value == mode }?.key
+    switch (mode) {
+        case 'off':
+            publish(powerOff())
+            break
+        case 'auto':
+        case 'cool':
+        case 'heat':
+            publish(powerOn([
+                'mode': mappedMode,
+                'temperature': convertInputToCelsius(getSetpointForMode(mode))
+            ]))
+            break
+        case 'fan':
+        case 'dry':
+            publish(powerOn(['mode': mappedMode]))
+            break
+    }
+}
+
+void setThermostatFanMode(String fanMode) {
+    switch (fanMode) {
+        case 'on':
+            // The fan is never "off", so we'll treat this command like
+            // "set the unit to fan mode" since otherwise it has no meaning
+            // Default the fan to whatever speed it was on previous run
+            publish(powerOn(['mode': 'FAN']))
+            break
+        case 'auto':
+        case 'circulate':
+            String mappedMode = thermostatFanModeMapping.find { m -> m.value == fanMode }?.key
+            publish(powerOn(['fan': mappedMode]))
+            break
+        case 'quiet':
+            publish(powerOn(['fan': 'QUIET']))
+            break
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+            publish(powerOn(['fan': fanMode]))
+            break
+    }
+}
+
+Map powerOn(Map command = [:]) {
+    ['power': 'ON'] + command
+}
+
+Map powerOff(Map command = [:]) {
+    ['power': 'OFF'] + command
+}
+
+/* MQTT */
+
+void connect() {
+    try {
+        logDebug "Connecting to MQTT broker at ${brokerIP}:${brokerPort}"
+        interfaces.mqtt.connect(getMQTTConnectURI(), "hubitat_${device.id}", brokerUsername, brokerPassword)
+    } catch (e) {
+        log.error "Error connecting to MQTT broker: ${e.message}"
+        reconnect()
+    }
+}
+
+void reconnect() {
+    state.connectDelay = state.connectDelay ?: 0
+    state.connectDelay++
+
+    runIn(state.connectDelay * 60, connect)
+}
+
+void disconnect() {
+    unsubscribe()
+    interfaces.mqtt.disconnect()
+}
+
+void publish(Map command) {
+    String payload = JsonOutput.toJson(command)
+    logDebug "Publishing ${payload} to ${getSetTopic()}"
+    interfaces.mqtt.publish(getSetTopic(), payload)
+}
+
+void subscribe() {
+    interfaces.mqtt.subscribe(topicRoot)
+    interfaces.mqtt.subscribe(getStatusTopic())
+}
+
+void unsubscribe() {
+    interfaces.mqtt.unsubscribe(topicRoot)
+    interfaces.mqtt.unsubscribe(getStatusTopic())
+}
+
+void mqttClientStatus(String status) {
+    logDebug status
+    if (status.startsWith('Error')) {
+        disconnect()
+        reconnect()
+    } else {
+        state.remove('connectDelay')
+        runIn(1, subscribe)
+    }
+}
+
+/* Helpers */
+
+String getMQTTConnectURI() {
+    "tcp://${brokerIP}:${brokerPort}"
+}
+
+String getStatusTopic() {
+    "${topicRoot}/status"
+}
+
+String getSetTopic() {
+    "${topicRoot}/set"
+}
+
+String getTemperatureUnit() {
+    "°${getTemperatureScale()}"
+}
+
+void disableDebugLogging() {
+    device.updateSetting('debugLoggingEnabled', [value: false, type: 'bool'])
+}
+
+void logDebug(String msg) {
+    if (debugLoggingEnabled) {
+        log.debug msg
+    }
+}
